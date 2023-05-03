084d2323ca770cf619b026f6c91e02c774449607
roksana-barna <roksanabarna1803@gmail.com>
2023-05-03 18:36:00 +0600

    json

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express =require ('express');
 const app =express();
+var cors = require('cors')
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('Chef is Coming')
 
 });
+app.get('/chefs', (req, res) =>{
+    console.log(chefs);
+    res.send(chefs);
+})
 app.listen(port,()=>{
     console.log(`chef api is runing on port : ${port}`)
 })
