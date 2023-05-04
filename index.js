4e04f3cb3b9a6fc434746bb40a07852743965ff2
roksana-barna <roksanabarna1803@gmail.com>
2023-05-04 15:36:41 +0600

    id

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express =require ('express');
 const app =express();
 var cors = require('cors')
 const port = process.env.PORT || 5000;
+
 const chefs = require('./data/chefs.json');
+
+const chefDetails = require('./data/details.json');
+const recipes =require('./data/recipes.json')
 
 app.use(cors())
 
@@ -10,10 +14,40 @@ app.get('/',(req,res)=>{
     res.send('Chef is Coming')
 
 });
+
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
+})
+
 app.get('/chefs', (req, res) =>{
-    console.log(chefs);
     res.send(chefs);
 })
+app.get('/chefdetails', (req, res) => {
+    res.send(chefDetails);
+})
+
+app.get('/chefdetails/:id', (req, res) => {
+    const id = req.params.id;
+    const selectchef = chefDetails.find(n=>n.id === id);
+    res.send(selectchef)
+})
+
+
+// app.get('/chefdetails/:id/recepies/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectchef = chefDetails.find(n=>n.id === id);
+//     res.send(selectchef)
+// })
+// app.get('/chefs/:id', (req, res) => {
+//     const id = (req.params.id);
+//     const chefsinfo =chefDetails.filter(n=>n.id===id)
+//       console.log(chefsinfo)  
+    
+//     else {
+//         const chefsinfo = chefDetails?.filter(n => parseInt(n.id) === id);
+//         res.send(chefsinfo)
+//     }
+    // })
 app.listen(port,()=>{
     console.log(`chef api is runing on port : ${port}`)
 })
